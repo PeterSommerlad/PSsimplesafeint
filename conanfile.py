fb17adc93ce2a024109c3cd347736545543ef7ec
Peter Sommerlad <peter.cpp@sommerlad.ch>
2021-04-20 18:21:34 +0200

    attempt further conan stuff...

--- conanfile.py
+++ conanfile.py
@@ -15,14 +15,12 @@ class PssimplesafeintConan(ConanFile):
     exports_sources = "include/*"
     # No settings/options are necessary, this is header only
 
-    # def source(self):
-    #     '''retrieval of the source code here. Remember you can also put the code
-    #     in the folder and use exports instead of retrieving it with this
-    #     source() method
-    #     '''
-    #     # self.run("git clone ...") or
-    #     # tools.download("url", "file.zip")
-    #     # tools.unzip("file.zip" )
+    def source(self):
+         '''retrieval of the source code here. Remember you can also put the code
+         in the folder and use exports instead of retrieving it with this
+         source() method
+         '''
+         self.run("git clone https://github.com/PeterSommerlad/PSsimplesafeint.git") 
 
     def package(self):
         self.copy("*.h")
